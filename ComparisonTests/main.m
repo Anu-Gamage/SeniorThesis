8a555f389bef89b78ce4eb09ad05ddf111dc14fb
Anu <anuththari@gmail.com>
2018-03-22 19:16:41 -0400

    Testing combine_C

--- ComparisonTests/main.m
+++ ComparisonTests/main.m
@@ -4,11 +4,11 @@
 clear;clc;close all
 
 n = 500;                        % no. of nodes 
-k = 2;                          % no. of clusters
+k = 3;                          % no. of clusters
 m_array = [1,2,3];                % no. of layers
 b = 0.02;                       % Tuning parameter for CRSP
 c = [2.0, 3.0, 4.0, 5.0, 6.0, 8.0, 10.0, 12.0,15.0, 20.0];               % Varying node degree
-lambda = 0.5;                   % For sbm-gen
+lambda = 0.7;                   % For sbm-gen
 lambda_scml = 0.5;              % regularization parameter for SC-ML
 do_plot = 0;                    % To plot data matrices
 do_result_plot = 1;             % To plot results
