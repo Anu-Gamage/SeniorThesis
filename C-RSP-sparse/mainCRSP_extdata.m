f7df87caadcf13fec01d3230868efeb5896b3281
Anu <anuththari@gmail.com>
2018-03-25 10:53:11 -0400

    more setup

--- C-RSP-sparse/mainCRSP_extdata.m
+++ C-RSP-sparse/mainCRSP_extdata.m
@@ -3,62 +3,39 @@
 % 3/25/2018
 clear;clc;close all
 
-n = 500;                        % no. of nodes 
-k = 2;                          % no. of clusters
-m_array = [1,2,3];                  % no. of layers
-b = 0.02;                       % Tuning parameter for RSP
-c = [2.0, 3.0, 4.0, 5.0, 6.0, 8.0, 10.0, 12.0,15.0, 20.0];               % Varying node degree
-lambda = 0.9;
-do_plot = 0;                     % To plot data matrices
+b = 0.02;                        % RSP parameter
 do_result_plot = 1;              % To plot results
-num_runs = 15;
-ccr_array = zeros(num_runs, numel(c), numel(m_array));
-nmi_array = zeros(num_runs, numel(c), numel(m_array));
 
+load('politicsuk_adjacency.mat')
+load('politicsuk_costs.mat')
+load('politicsuk_labels.mat')
+A = politicsuk_adjacency;
+C = politics_uk_costs;
+labels = politicsuk_labels;
 
-for runs = 1:num_runs
-    % Generate adjacency tensor of test data
-    data = cell(1,numel(c));   
-    for i = 1:numel(c)
-        [data{i}, labels] = mlsbm_gen(n,k,max(m_array), c(i), lambda);
-    end
-    for layers = 1:numel(m_array)        % Varying no. of layers
-        m = m_array(layers);
-        acc = zeros(1, numel(c));
-        nmi = zeros(1, numel(c));
-        for degree = 1:numel(c)         % Varying node degree
-            A = cell(1,layers);
-            for i = 1:m                 % Select relevant tensor
-                A{i} = data{degree}{i};
-            end
-            fprintf('Variable %d processing:\n', degree)
-            if do_plot
-               figure; 
-               for i = 1:m
-                  subplot(1,m,i);spy(A{i}); title(sprintf('Layer %d', i))
-               end
-            end
+n = size(A{1},1);
+m = numel(A);
+k = 5;
+ccr_array = zeros(1,m);
+nmi_array = zeros(1,m);
+m_array = 1:m;
+
+for layers = 1:m
             % Run CRSP
-            [acc(degree), nmi(degree), final_labels] = CRSP(A,A,labels', n, k,m, b); % Cost matrix = A
-            fprintf('CCR: %.2f\n', acc(degree))
-            fprintf('NMI: %.2f\n\n', nmi(degree))
-        end
-        ccr_array(runs,:, layers) = acc;
-        nmi_array(runs,:, layers) = nmi;
-    end
+            [ccr_array(i),nmi_array(i), final_labels] = CRSP(A,C,labels, n, k, m, b); 
+            fprintf('CCR: %.2f\n', ccr_array(i))
+            fprintf('NMI: %.2f\n\n', nmi_array(i))
 end
+    
+
 
 if do_result_plot
  for i = 1:numel(m_array)
-        avg_ccr = mean(ccr_array(:,:,i));
-        avg_nmi = mean(nmi_array(:,:,i));
-        std_ccr = std(ccr_array(:,:,i));
-        std_nmi = std(nmi_array(:,:,i));
-        figure;yyaxis left; errorbar(c, avg_ccr, std_ccr); ylabel('CCR'); ylim([ 0,100])
-        hold on;yyaxis right; errorbar(c, avg_nmi, std_nmi); ylabel('NMI'); ylim([0,1])
-        title(sprintf('CRSP: Nodes = %d, Clusters = %d, Layers = %d, Runs=%d',n, k,m_array(i), num_runs))
-        xlabel('c'); 
+        figure;yyaxis left; plot(m_array, ccr_array); ylabel('CCR'); ylim([ 50,100])
+        hold on;yyaxis right; plot(m_array, nmi_array); ylabel('NMI'); ylim([0,1])
+        title(sprintf('CRSP: Nodes = %d, Clusters = %d, Layers = %d',n,k, m))
+        xlabel('Layers'); 
         legend('CCR', 'NMI','Location','SouthEast')
- saveas(gcf, [pwd '/figs/' sprintf('n%d_k%d_m%d_r%d.png', n,k,i,num_runs)])
+ %saveas(gcf, [pwd '/figs/' sprintf('n%d_k%d_m%d_r%d.png', n,k,i,num_runs)])
  end
 end
