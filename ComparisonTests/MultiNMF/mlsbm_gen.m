9baae19462a783f47c5a5234f3a76935a7e2b1c9
Anuththari Gamage <anuththari@gmail.com>
2018-03-30 19:57:45 -0400

    Add files via upload

---
+++ ComparisonTests/MultiNMF/mlsbm_gen.m
@@ -0,0 +1,37 @@
+function [A, labels] = mlsbm_gen(n,k,m,cin,lambda)
+% Generates multi-layers SBM graphs
+% Inputs:
+% n - number of nodes
+% k - number of communities
+% m - number of layers
+% cin - intra-cluster node degree
+% lambda - intra-cluster node degree
+% Outputs: 
+% A - cell array of SBM graphs
+% labels - cluster assignment for each node
+% Anuththari Gamage, 3/16/2018 
+
+    A = cell(1,m);
+    cout = (1-lambda)*cin;
+    
+    tm = clock;                         % Shuffle random numbers
+    rng(round(tm(6)), 'twister');
+    
+    for layers = 1:m
+        seed = randi(1000);
+        [G,labels] = sbm_gen(n,k,cin,cout,seed);
+
+        % Check for isolated nodes and fix
+        isol = find(sum(G, 2) == 0);
+        if ~isempty(isol)
+            for idx = 1:numel(isol)
+               neighbors = find(labels == labels(isol(idx)));
+               edge = datasample(neighbors, 1);
+               G(isol(idx),edge) = 1;
+               G(edge, isol(idx)) = 1;
+            end
+        end
+
+        A{layers} = G;
+    end
+end
